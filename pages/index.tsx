59d610ff5c3fbb5586911ce9612e47193d7b4eba
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-23 12:57:27 +0100

    improvements

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
+import {} from "@app/prelude"
 import * as Sy from "@effect-ts/core/Sync"
 import * as Sl from "@effect-ts/core/Sync/Layer"
 
 import { Home, LiveHome } from "../components/Home"
-import {} from "../src/prelude"
 
 export const HomePage = Sy.accessService(Home)((_) => _.HomeView)
   ["|>"](Sl.provideSyncLayer(LiveHome))

453c7943d557747d32bb1a2c75a507afbdfd5bd0
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-23 13:07:06 +0100

    use matcher

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -1,8 +1,10 @@
+import {} from "@app/prelude"
 import * as As from "@effect-ts/core/Async"
 import { tag } from "@effect-ts/core/Has"
 import * as Sy from "@effect-ts/core/Sync"
 import * as Sl from "@effect-ts/core/Sync/Layer"
-import type { _A, _E } from "@effect-ts/core/Utils"
+import type { _A } from "@effect-ts/core/Utils"
+import { _E, matchTag } from "@effect-ts/core/Utils"
 import { observer } from "mobx-react"
 import React from "react"
 
@@ -34,7 +36,7 @@ export const makeHome = Sy.gen(function* (_) {
     return <div>Loading</div>
   }
 
-  function Error<E>(p: { e: State.Error<E> }) {
+  function Error<E>(p: { e: E }) {
     return <div>Error: {JSON.stringify(p.e)}</div>
   }
 
@@ -49,23 +51,16 @@ export const makeHome = Sy.gen(function* (_) {
           return next()
         }
       }, [])
-      switch (state.current._tag) {
-        case "Done": {
-          return <Done orgs={state.current.value} />
-        }
-        case "Error": {
-          return <Error e={state.current} />
-        }
-        case "Init": {
-          return <Init />
-        }
-        case "Loading": {
-          return <Loading />
-        }
-        case "Interrupted": {
-          return <Interrupted />
-        }
-      }
+
+      return state.current["|>"](
+        matchTag({
+          Done: ({ value }) => <Done orgs={value} />,
+          Error: ({ error }) => <Error e={error} />,
+          Init: () => <Init />,
+          Interrupted: () => <Interrupted />,
+          Loading: () => <Loading />
+        })
+      )
     })
   }
 })
